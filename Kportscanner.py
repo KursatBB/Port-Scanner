75aa7668b208e331ca9e6080cf5878f96f79483f
Kürşat <kursat171717@hotmail.com>
2023-02-07 18:50:44 +0300

    Service names added
    Added most known Port-Service pairings.

--- Kportscanner.py
+++ Kportscanner.py
@@ -3,12 +3,20 @@ import threading
 import pyfiglet
 ascii_banner = pyfiglet.figlet_format("KeK  PortScan")
 
+def get_service_name(port):
+    try:
+        return socket.getservbyport(port)
+    except socket.error:
+        return "Unknown service"
 def scan_port(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
     try:
         con = s.connect((host, port))
-        print(f'Port {port} is open.')
+        servName= get_service_name(port)
+        if port==8080: servName ='http'
+        elif port==8443: servName ='https'
+        print(f'Port {port} is open. Service name is --> ',servName)
         con.close()
     except:
         pass
@@ -30,3 +38,5 @@ if __name__ == '__main__':
     threadnum = int(input("How much thread do you want : "))
     portnum=int(input("Which port will you scan until? : "))
     main(host,threadnum,portnum)
+    
+    
